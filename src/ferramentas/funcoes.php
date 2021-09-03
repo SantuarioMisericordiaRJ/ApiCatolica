c73305e417a5c87a579b852efaa87c75382bbf46
Protocol Corporation <protocollive@outlook.com>
2021-09-03 16:17:51 -0300

    Optional parameter, fix division and header

--- src/ferramentas/funcoes.php
+++ src/ferramentas/funcoes.php
@@ -1,14 +1,20 @@
 <?php
+//2021.09.03.00
+//Protocol Corporation Ltda.
+//https://github.com/SantuarioMisericordiaRJ/ApiCatolica
 
-function AnoLetra(int $Ano):?string{
-    $temp = $Ano % 3;
-    if($temp === 0):
-        return 'C';
-    elseif($temp === 1):
-        return 'A';
-    elseif($temp === 2):
-        return 'B';
-    else:
-        return null;
-    endif;
+function AnoLetra(int $Ano = null):?string{
+  if($Ano === null):
+    $Ano = time();
+  endif;
+  $temp = date('Y', $Ano) % 3;
+  if($temp === 0):
+    return 'C';
+  elseif($temp === 1):
+    return 'A';
+  elseif($temp === 2):
+    return 'B';
+  else:
+    return null;
+  endif;
 }
