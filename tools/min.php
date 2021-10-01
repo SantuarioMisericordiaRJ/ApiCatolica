8bd67d97c7eb873e297604a68a955e72461c71c0
Protocol Corporation <protocollive@outlook.com>
2021-10-01 13:10:43 -0300

    Arquivo excluído

--- tools/min.php
+++
@@ -1,8 +0,0 @@
-<?php
-$temp = file_get_contents(dirname(__DIR__, 1) . '/src/index.json');
-$temp = str_replace(['  ', "\n", "\r"], '', $temp);
-file_put_contents(dirname(__DIR__, 1) . '/src/index-min.json', $temp);
-
-$temp = file_get_contents(dirname(__DIR__, 1) . '/src/especiais.json');
-$temp = str_replace(['  ', "\n", "\r"], '', $temp);
-file_put_contents(dirname(__DIR__, 1) . '/src/especiais-min.json', $temp);
