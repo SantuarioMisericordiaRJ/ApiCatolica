9d314849d037b948a2c5fd2babc5da4c9e92fec9
Protocol Corporation <protocollive@outlook.com>
2021-10-03 10:06:52 -0300

    Retornando letras em minúscilo

--- tools/funcoes.php
+++ tools/funcoes.php
@@ -1,5 +1,5 @@
 <?php
-//2021.09.03.00
+//2021.10.03.00
 //Protocol Corporation Ltda.
 //https://github.com/SantuarioMisericordiaRJ/ApiCatolica
 
@@ -9,11 +9,11 @@ function AnoLetra(int $Ano = null):?string{
   endif;
   $temp = date('Y', $Ano) % 3;
   if($temp === 0):
-    return 'C';
+    return 'c';
   elseif($temp === 1):
-    return 'A';
+    return 'a';
   elseif($temp === 2):
-    return 'B';
+    return 'b';
   else:
     return null;
   endif;
