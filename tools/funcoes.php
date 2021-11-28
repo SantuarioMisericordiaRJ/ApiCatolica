8cb2f89188ed2292186fbb8bde7de441b3980422
Protocol Corporation <protocollive@outlook.com>
2021-11-28 19:58:17 -0300

    $Ano agora é obrigatório, parâmetro para identificar o tempo

--- tools/funcoes.php
+++ tools/funcoes.php
@@ -1,11 +1,15 @@
 <?php
-//2021.10.03.00
+//2021.11.28.00
 //Protocol Corporation Ltda.
 //https://github.com/SantuarioMisericordiaRJ/ApiCatolica
 
-function AnoLetra(int $Ano = null):?string{
-  if($Ano === null):
-    $Ano = time();
+/**
+ * @param int $Ano Timestamp
+ * @param string $Tempo A identificação do tempo litúrgico, usando as constantes da classe AnoLiturgico
+ */
+function AnoLetra(int $Ano, int $Tempo = null):?string{
+  if($Tempo === AnoLiturgico::TempoAdvento):
+    $Ano = strtotime('+1 year', $Ano);
   endif;
   $temp = date('Y', $Ano) % 3;
   if($temp === 0):
